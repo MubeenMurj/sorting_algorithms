edae03eb7a0d8337266b0c28bab767d56a0dd81c
Mubeen Rehman <mrjazib1@gmail.com>
2022-10-23 20:07:12 +0500

    Java: Bubble sort

---
+++ java/bubble_sort.java
@@ -0,0 +1,19 @@
+static int[] bubble_sort(int[] arr) {
+  boolean noSwaps;
+  for (int i = 0; i < arr.length; i++) {
+    noSwaps = true;
+    for (int j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) {
+      break;
+    }
+  }
+
+  return arr;
+}
