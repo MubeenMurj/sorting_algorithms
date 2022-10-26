e33ba6ae5c323bcfb830a361a6845c2e171ee11a
Mubeen Rehman <mrjazib1@gmail.com>
2022-10-26 19:47:19 +0500

    C++: Selection Sort

---
+++ cpp/selection_sort.cpp
@@ -0,0 +1,19 @@
+int *selection_sort (int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    int min = i;
+    for (int j = i + 1; j < size; j++)
+    {
+      if (arr[j] < arr[min])
+      {
+        min = j;
+      }
+    }
+    int temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+
+  return arr;
+}
