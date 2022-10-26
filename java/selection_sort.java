d4480fe59918ad1cc1150207132b446391f494cf
Mubeen Rehman <mrjazib1@gmail.com>
2022-10-26 19:47:58 +0500

    Java: Selection Sort

---
+++ java/selection_sort.java
@@ -0,0 +1,17 @@
+static int[] selection_sort(int[] arr) {
+  for (int i = 0; i < arr.length; i++) {
+    int min = i;
+    for (int j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      int temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+
+  return arr;
+}
